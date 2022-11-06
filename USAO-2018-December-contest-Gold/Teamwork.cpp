8277fb41748d2145bd044167599668a0a612023e
ss77ii <80611722+ss77ii@users.noreply.github.com>
2022-11-06 14:50:44 +0000

    11-06

---
+++ USAO-2018-December-contest-Gold/Teamwork.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int inp[10002];
+int dp[10002];
+
+int main()
+{
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; i++)
+        cin >> inp[i];
+    
+    dp[1] = inp[1];
+
+    for (int i = 2; i <= n; i++)
+    {
+        int Max = inp[i];
+        for (int j = i; j >= 1 && (i - j + 1 <= k); j--)
+        {
+            Max = max(Max, inp[j]);
+            dp[i] = max(dp[i], dp[j - 1] + Max * (i - j + 1));
+        }
+    }
+
+    cout << dp[n] << endl;
+
+    return 0;
+}
