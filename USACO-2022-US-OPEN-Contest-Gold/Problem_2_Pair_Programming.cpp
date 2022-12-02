41a111a6f73f98afb1af6887803f95d428a5bf94
ss77ii <ziruiwang.spencer@gmail.com>
2022-12-02 07:54:05 -0500

    2022-12-2

--- USACO-2022-US-OPEN-Contest-Gold/Problem_2_Pair_Programming.cpp
+++ USACO-2022-US-OPEN-Contest-Gold/Problem_2_Pair_Programming.cpp
@@ -1,9 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int t;
+int MOD = 1000000007;
+
+string get()
+{
+    string s, t;
+    cin >> s;
+    for (char c : s)
+	{
+        if (c == '0')
+		{
+            t = "0";
+        }
+		else if (c != '1')
+		{
+            t += c;
+        }
+    }
+    return t;
+}
+
+int f[2005][2005];
+int g[2005][2005];
+
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
 	
+	cin >> t;
+
+	int n;
+	string x, y;
+	for (int a = 1; a <= t; a++)
+	{
+		cin >> n;
+		
+		x = get();
+		y = get();
+		
+		int ans = 0;
+
+		memset(f, 0, sizeof(f));
+    	memset(g, 0, sizeof(g));
+
+	    f[1][0] = 1;
+    	g[0][1] = 1;
+
+    	for (int i = 0; i <= x.size(); i++)
+    	{
+    		for (int j = 0; j <= y.size(); j++)
+    		{
+    			if (i == 0 && j == 0)
+					continue;
+
+				if (i < x.size())
+				{
+					f[i + 1][j] = (f[i + 1][j] + f[i][j]) % MOD;
+					if (j > 0 && y[j - 1] != '+' && x[i] == '+')
+						f[i + 1][j] = (f[i + 1][j] + g[i][j]) % MOD;
+					if (j > 0 && y[j - 1] == '+' && x[i] != '+')
+						f[i + 1][j] = (f[i + 1][j] + g[i][j]) % MOD;
+				}
+
+				if (j < y.size())
+				{
+					g[i][j + 1] = (g[i][j + 1] + g[i][j]) % MOD;
+            	    g[i][j + 1] = (g[i][j + 1] + f[i][j]) % MOD;
+				}
+			}
+		}
+
+		ans = (f[x.size()][y.size()] + g[x.size()][y.size()]);
+		
+		cout << (ans % MOD) << endl;
+	}
+
 	return 0;
 }
 
